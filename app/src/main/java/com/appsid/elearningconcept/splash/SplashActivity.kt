adf2a1d93c5094ffe28c8579b8c0a350695a06fe
Mycho Aditya <mychoaditya04@gmail.com>
2022-02-21 00:41:05 +0700

    commit

---
+++ app/src/main/java/com/appsid/elearningconcept/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.appsid.elearningconcept.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.appsid.elearningconcept.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+    }
+}
